fc83fceb434a66adca344e43e40041fce0bcafa4
Ayman Zahir <58708555+AymanZahir@users.noreply.github.com>
2025-11-25 18:05:47 -0500

    Correction UI

--- projet-phase2/src/main/java/com/brokerx/config/SecurityConfig.java
+++ projet-phase2/src/main/java/com/brokerx/config/SecurityConfig.java
@@ -37,6 +37,9 @@ public class SecurityConfig {
                     .requestMatchers(
                         "/api/v1/auth/**",
                         "/api/v1/market/**",
+                        "/",
+                        "/index.html",
+                        "/static/**",
                         "/swagger-ui.html",
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
